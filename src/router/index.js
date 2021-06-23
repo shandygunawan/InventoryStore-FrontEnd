f6122977adbd9153ac130081dca30c1ec65502f2
Shandy G <shandy.gunawan2109@gmail.com>
2021-06-23 20:32:54 +0700

    Add meta config to all routes

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,6 @@ const routes = [
     meta: { requiresUnauth: true },
     beforeEnter: (to, from, next) => {
       if (store.getters.isAuthenticated) {
-        console.log("before enter");
         next('/products');
       }
       else {
@@ -29,51 +28,87 @@ const routes = [
   {
     path: '/dashboard',
     name: 'dashboard',
-    component: () => import('../pages/Dashboard.vue')
+    component: () => import('../pages/Dashboard.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
 
   // LIST
   {
     path: '/suppliers',
     name: 'list-suppliers',
-    component: () => import('../pages/ListSuppliers.vue')
+    component: () => import('../pages/ListSuppliers.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/buyers',
     name: 'list-buyers',
-    component: () => import('../pages/ListBuyers.vue')
+    component: () => import('../pages/ListBuyers.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/products',
     name: 'list-products',
-    component: () => import('../pages/ListProducts.vue')
+    component: () => import('../pages/ListProducts.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/incomings',
     name: 'list-incomings',
-    component: () => import('../pages/ListIncomings.vue')
+    component: () => import('../pages/ListIncomings.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/outgoings',
     name: 'list-outgoings',
-    component: () => import('../pages/ListOutgoings.vue')
+    component: () => import('../pages/ListOutgoings.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
 
   // DETAIL
   {
     path: '/products/:product_id/',
     name: "detail-product",
-    component: () => import('../pages/DetailProduct.vue')
+    component: () => import('../pages/DetailProduct.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/incomings/:incoming_id/',
     name: "detail-incoming",
-    component: () => import('../pages/DetailIncoming.vue')
+    component: () => import('../pages/DetailIncoming.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
   {
     path: '/outgoings/:outgoing_id/',
     name: "detail-outgoing",
-    component: () => import('../pages/DetailOutgoing.vue')
+    component: () => import('../pages/DetailOutgoing.vue'),
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin']
+    },
   },
 
 
@@ -82,31 +117,53 @@ const routes = [
     path: '/input/incoming',
     name: 'input-incoming',
     component: () => import('../pages/InputIncoming.vue'),
-    meta: { requiresAuth: true },
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin', 'input']
+    },
   },
   {
     path: '/input/outgoing',
     name: 'input-outgoing',
     component: () => import('../pages/InputOutgoing.vue'),
-    meta: { requiresAuth: true },
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin', 'input']
+    },
   },
   {
     path: '/input/supplier',
     name: 'input-supplier',
     component: () => import('../pages/InputSupplier.vue'),
-    meta: { requiresAuth: true },
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin', 'input']
+    },
   },
   {
     path: '/input/buyer',
     name: 'input-buyer',
     component: () => import('../pages/InputBuyer.vue'),
-    meta: { requiresAuth: true },
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin', 'input']
+    },
   },
   {
     path: '/input/product',
     name: 'input-product',
     component: () => import('../pages/InputProduct.vue'),
-    meta: { requiresAuth: true },
+    meta: { 
+      requiresAuth: true,
+      allowedRoles: ['admin', 'input']
+    },
+  },
+
+  // OTHER
+  {
+    path: '/notallowed',
+    name: 'other-notallowed',
+    component: () => import('../pages/OtherNotAllowed.vue'),
   }
 ]
 
@@ -119,12 +176,12 @@ const router = new VueRouter({
 store.dispatch('autoLogin');
 
 router.beforeEach(function(to, _, next) {
-  console.log(to.meta.requiresUnauth);
-  console.log(store.getters.isAuthenticated);
+  const role = store.getters.role;
 
-  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
-    console.log("auth");
+  if ( to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next('/login');
+  } else if ( to.meta.requiresAuth && !to.meta.allowedRoles.includes(role) ) {
+    next('/notallowed');
   } else {
     next();
   }
