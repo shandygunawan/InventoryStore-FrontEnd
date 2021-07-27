bda87d13f7a0491b0505c216a9ff47160dc63698
shandygunawan <shandy.development@gmail.com>
2021-07-27 14:48:56 +0700

    merge backup and restore into one cards, integrate with dropbox backend

--- src/pages/AdminDatabase.vue
+++ src/pages/AdminDatabase.vue
@@ -42,6 +42,7 @@
         </v-col>
       </v-row>
 
+      <!-- Backup and Restore -->
       <v-row>
         <v-col class="col-12">
           <v-card>
@@ -50,17 +51,17 @@
               dark
               flat
             >
-              <v-toolbar-title>Backup</v-toolbar-title>
+              <v-toolbar-title>Backup And Restore</v-toolbar-title>
 
               <template v-slot:extension>
                 <v-tabs
-                  v-model="tabs.backup_state"
+                  v-model="tabs.backuprestore_state"
                   align-with-title
                 >
                   <v-tabs-slider color="yellow"></v-tabs-slider>
 
                   <v-tab
-                    v-for="item in tabs.backup_items"
+                    v-for="item in tabs.backuprestore_items"
                     :key="item"
                   >
                     {{ item }}
@@ -69,37 +70,26 @@
               </template>
             </v-toolbar>
 
-            <v-tabs-items v-model="tabs.backup_state">
-
-              <!-- Lokasi -->
-              <v-tab-item>
-                <v-simple-table>
-                  <thead>
-                    <tr>
-                      <th>Lokasi Backup</th>
-                      <th>Waktu Backup Terakhir</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Server</td>
-                      <td>
-                        {{ toLocaleDateTime(backup_info.time_createdbackup_latest_server) }}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Dropbox</td>
-                      <td>21 September 2021</td>
-                    </tr>
-                  </tbody>
-                </v-simple-table>
-              </v-tab-item>
+            <v-tabs-items v-model="tabs.backuprestore_state">
 
               <!-- Sejarah -->
               <v-tab-item>
                 <v-card>
                   <v-card-text>
-                    Test
+                    <v-data-table
+                      :headers="backup_history.headers"
+                      :items="backup_history.data"
+                      :item-key="backup_history.data.name"
+                      :items-per-page="5"
+                      must-sort="true"
+                      sort-by="name"
+                      class="elevation-1"
+                    >
+                      <template v-slot:item.datetime="{ item }">
+                        {{ toLocaleDateTime(item.datetime) }}
+                      </template>
+
+                    </v-data-table>
                   </v-card-text>
                 </v-card>
               </v-tab-item>
@@ -109,10 +99,19 @@
                 <v-card>
                   <v-card-text>
                     <v-row>
-                      <v-col class="col-12 col-sm-6 col-md-4">
+                      <v-col class="col-12 col-sm-4 col-md-3">
                         <v-btn
                           color="cyan white--text"
-                        >Backup Sekarang</v-btn>
+                        >
+                          Backup Sekarang
+                        </v-btn>
+                      </v-col>
+                      <v-col class="col-12 col-sm-4 col-md-3">
+                        <v-btn
+                          color="green white--text"
+                        >
+                          Restore Otomatis
+                        </v-btn>
                       </v-col>
                     </v-row>
                   </v-card-text>
@@ -156,24 +155,6 @@
                         </v-menu>
                       </v-col>
                     </v-row>
-
-                    <v-row>
-                      <v-col class="col-6">
-                        <span>Tujuan Backup</span>
-                        <v-checkbox
-                          v-model="form_backup.backup_target"
-                          label="Server"
-                          value="server"
-                          hide-details
-                        ></v-checkbox>
-                        <v-checkbox
-                          v-model="form_backup.backup_target"
-                          label="Dropbox"
-                          value="dropbox"
-                          hide-details
-                        ></v-checkbox>
-                      </v-col>
-                    </v-row>
                   </v-card-text>
                   <v-card-actions>
                     <v-spacer></v-spacer>
@@ -188,61 +169,6 @@
         </v-col>
       </v-row>
 
-      <!-- Restore -->
-      <v-row>
-        <v-col class="col-12">
-          <v-card>
-            <v-toolbar
-              flat
-              color="green"
-              dark
-            >
-              <v-toolbar-title>Restore</v-toolbar-title>
-
-              <template v-slot:extension>
-                <v-tabs
-                  v-model="tabs.restore_state"
-                  align-with-title
-                >
-                  <v-tabs-slider color="red"></v-tabs-slider>
-
-                  <v-tab
-                    v-for="item in tabs.restore_items"
-                    :key="item"
-                  >
-                    {{ item }}
-                  </v-tab>
-                </v-tabs>
-              </template>
-            </v-toolbar>
-
-            <v-tabs-items v-model="tabs.restore_state">
-              <v-tab-item>
-                <v-card flat>
-                  <v-card-text>
-                    <v-btn color="green white--text">Restore</v-btn>
-                  </v-card-text>
-                </v-card>
-              </v-tab-item>
-              <v-tab-item>
-                <v-card flat>
-                  <v-card-text>
-                    <v-file-input
-                      show-size
-                      truncate-length="100"
-                      label="Upload Dump DB"
-                    ></v-file-input>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn text color="green">Restore</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-tab-item>
-            </v-tabs-items>
-          </v-card>
-        </v-col>
-      </v-row>
     </v-container>
   </div>
 </template>
@@ -255,15 +181,19 @@ export default {
     return {
       menu_backup_time: null,
       backup_info: {},
+      backup_history: {
+        data: [],
+        headers: [
+          { text: "Nama Backup", value: "name", sortable: true },
+          { text: "Waktu Backup", value: "datetime", sortable: true }
+        ]
+      },
       tabs: {
-        backup_state: null,
-        backup_items: [ "Lokasi", "Sejarah", "Aksi", "Pengaturan" ],
-        restore_state: null,
-        restore_items: [ "Automatis", "Manual" ],
+        backuprestore_state: null,
+        backuprestore_items: [ "Sejarah", "Aksi", "Pengaturan" ],
       },
       form_backup: {
         backup_time: null,
-        backup_target: [],
       }
     };
   },
@@ -279,12 +209,19 @@ export default {
         .then((response) => {
           this.backup_info = response.data.data;
           this.form_backup.backup_time = this.backup_info.time_autobackup;
-          this.form_backup.backup_target = this.backup_info.location_backup;
+        })
+    },
+    getBackupHistory() {
+      axios.get("utils/backup/list")
+        .then((response) => {
+          console.log(response);
+          this.backup_history.data = response.data.data;
         })
     }
   },
   created() {
     this.getBackupInfo();
+    this.getBackupHistory();
   }
 }
 </script>
